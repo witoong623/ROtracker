e4fd9a94eb3c2efd1ce82ad0563151c8da42f303
Witoon <alive-@msn.com>
2014-10-16 19:00:40 +0700

    Initial Main form

---
+++ src/frmMain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Windows.Forms;
+
+class frmMain : Form
+{
+    static void Main()
+    {
+        frmMain main = new frmMain();
+        Application.EnableVisualStyles();
+        Application.Run(main);
+    }
+}
