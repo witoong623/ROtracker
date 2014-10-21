4a1baa8f9836a8b5f1589eb2d5e6f288e0e0a31a
Witoon <alive-@msn.com>
2014-10-21 22:20:48 +0700

    Add DBConnector Constructor

--- src/DBConnector.cs
+++ src/DBConnector.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace ROtracker.src
 {
     class DBConnector
     {
+        private MySqlConnection Connection;
 
+        public DBConnector()
+        {
+
+        }
     }
 }
