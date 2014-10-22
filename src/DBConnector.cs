5d56be350a42a032f7097e9764a294d9e660d7b7
Witoon <alive-@msn.com>
2014-10-22 17:56:17 +0700

    Add CloseConnection method to DBConnector

--- src/DBConnector.cs
+++ src/DBConnector.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using MySql.Data.MySqlClient;
 using FileManage;
 
@@ -14,7 +15,55 @@ namespace ROtracker.src
 
         public DBConnector()
         {
-            
+            var buid = new clsBuildConnectionString("sql.txt");
+
+            if (buid.BuildConnectionString())
+            {
+                Connection = new MySqlConnection(buid.ConnectionString);
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        public bool OpenConnection()
+        {
+            try
+            {
+                if (Connection.State == System.Data.ConnectionState.Open)
+                {
+                    return true;
+                }
+                else
+                {
+                    Connection.Open();
+                    return true;
+                }
+            }
+            catch (NullReferenceException)
+            {
+                MessageBox.Show("Cannot open connection because cannot build connection string",
+                    "Cannot build connection string", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message, ex.Number.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
+        public void CloseConnection()
+        {
+            try
+            {
+                Connection.Close();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message + ex.StackTrace, ex.Number.ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
