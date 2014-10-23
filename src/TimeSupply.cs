7215354b01bda49684877f16b33c021e7a44e568
Witoon <alive-@msn.com>
2014-10-23 17:34:25 +0700

    Add TimeSupply class

---
+++ src/TimeSupply.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROtracker.src
+{
+    class TimeSupply
+    {
+        private string[] notificationString;
+        private double nextTimeBorn;
+        private DBConnector Database;
+
+        public TimeSupply()
+        {
+            Database = new DBConnector();
+        }
+
+        public string[] NotificationString
+        {
+            get
+            {
+                return notificationString;
+            }
+        }
+
+        public int NextTimeBorn
+        {
+            get
+            {
+                return (int)nextTimeBorn;
+            }
+        }
+    }
+}
