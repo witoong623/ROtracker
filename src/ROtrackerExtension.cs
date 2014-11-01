588b5881c4b4fd39ee590e2bfaa77a29c10e1f0c
Witoon <alive-@msn.com>
2014-11-02 00:21:21 +0700

    Update RoundDetail class

--- src/ROtrackerExtension.cs
+++ src/ROtrackerExtension.cs
@@ -9,16 +9,28 @@ namespace ROtracker.Extension
     public class RoundDetail
     {
         public string Name { get; private set; }
-        public DateTime DeadTime { get; private set; }
         public string Map { get; private set; }
+        public double Interval { get; private set; }
         public int[] Cordinate { get; private set; }
+        public DateTime DeadTime { get; private set; }
+        public DateTime NextTime { get; private set; }
+        public TimeSpan TimeRemaining { get; private set; }
+        public int Hours { get; private set; }
+        public int Minutes { get; private set; }
+        public int Secounds { get; private set; }
 
-        public RoundDetail(string name, DateTime deadTime, string map, int[] cordinate)
+        public RoundDetail(string name, double interval, DateTime deadTime, string map, int[] cordinate)
         {
             Name = name;
+            Interval = interval;
             DeadTime = deadTime;
             Map = map;
             Cordinate = cordinate;
+            NextTime = DeadTime.AddMinutes(Interval);
+            TimeRemaining = NextTime.Subtract(DeadTime);
+            Hours = TimeRemaining.Hours;
+            Minutes = TimeRemaining.Minutes;
+            Secounds = TimeRemaining.Seconds;
         }
     }
 }
